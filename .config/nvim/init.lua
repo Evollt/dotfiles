5e6dcc9655d23e90ad3c534e5127ba5ed063881c
Evollt <levniko343@gmail.com>
2024-06-01 19:08:02 +0300

    Add all my configs

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.opt.guicursor = "n-v-c:block-Cursor/lCursor,ve:ver35-Cursor,o:hor50-Cursor,i-ci:ver25-Cursor,r-cr:hor20-Cursor"
+
+vim.o.spell = false
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,119 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "selene",
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "vue-language-server",
+        "css-lsp",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local vue_typescript_plugin = require("mason-registry").get_package("vue-language-server"):get_install_path()
+        .. "/node_modules/@vue/language-server"
+        .. "/node_modules/@vue/typescript-plugin"
+
+      opts.servers = vim.tbl_deep_extend("force", opts.servers, {
+        volar = {},
+        -- Volar 2.0 has discontinued their "take over mode" which in previous version provided support for typescript in vue files.
+        -- The new approach to get typescript support involves using the typescript language server along side volar.
+        tsserver = {
+          init_options = {
+            plugins = {
+              -- Use typescript language server along with vue typescript plugin
+              {
+                name = "@vue/typescript-plugin",
+                location = vue_typescript_plugin,
+                languages = { "javascript", "typescript", "vue" },
+              },
+            },
+          },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "vue",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      window = {
+        position = "float", -- Используем плавающий режим
+        popup = {
+          -- Настройка для плавающего окна
+          size = {
+            height = "60%", -- высота в процентах от окна редактора
+            width = "30%", -- ширина в процентах от окна редактора
+          },
+          position = "50%", -- позиционирование по центру (вертикальное и горизонтальное)
+        },
+      },
+    },
+  },
+
+  "mg979/vim-visual-multi",
+  "tpope/vim-rails",
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      local mason = require("mason")
+
+      local mason_tool_installer = require("mason-tool-installer")
+
+      -- enable mason and configure icons
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "standardrb",
+          "prettier",
+          "prettierd",
+          "ktlint",
+          "eslint_d",
+          "google-java-format",
+          "htmlbeautifier",
+          "beautysh",
+          "buf",
+          "rustfmt",
+          "yamlfix",
+          "taplo",
+          "shellcheck",
+        },
+      })
+    end,
+  },
+}
